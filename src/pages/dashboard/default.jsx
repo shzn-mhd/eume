94d0346498f509ffbc5010e58bee90b1bc1f235d
Malisha <it20163372@my.sliit.lk>
2024-06-12 12:14:07 +0530

    language change - dashboard heading

--- src/pages/dashboard/default.jsx
+++ src/pages/dashboard/default.jsx
@@ -171,7 +171,7 @@ const DashboardDefault = ({
     <Grid container rowSpacing={4.5} columnSpacing={2.75}>
       {/* row 1 */}
       <Grid item xs={12} sx={{ mb: -2.25 }}>
-        <Typography variant="h5">Dashboard</Typography>
+        <Typography variant="h5">{t("Dashboard")}</Typography>
       </Grid>
       <Grid item xs={12} sm={6} md={4} lg={3}>
         <div style={{marginBottom: '20px'}}>
