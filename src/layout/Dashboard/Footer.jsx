8417594c8efd9ade544a8900c8cf5098f7ef83da
Malisha <it20163372@my.sliit.lk>
2024-06-24 22:32:15 +0530

    csv import and footer logo

--- src/layout/Dashboard/Footer.jsx
+++ src/layout/Dashboard/Footer.jsx
@@ -3,10 +3,13 @@ import { Link as RouterLink } from 'react-router-dom';
 // material-ui
 import { Link, Stack, Typography } from '@mui/material';
 
+import footerImg from 'assets/images/eume-logo-bottom.png'
+
 const Footer = () => (
   <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ p: '24px 16px 0px', mt: 'auto' }}>
     <Typography variant="caption">&copy; All rights reserved by EUME</Typography>
     <Stack spacing={1.5} direction="row" justifyContent="space-between" alignItems="center">
+      <img src={footerImg} alt="EUME-logo" />
       {/* <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
         About us
       </Link> */}
