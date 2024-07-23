4b7372bd1001232c06895ca995db0b1ecfe9aba5
Malisha <68454239+malishaw@users.noreply.github.com>
2024-07-23 16:05:34 +0530

    Update Footer.jsx

--- src/layout/Dashboard/Footer.jsx
+++ src/layout/Dashboard/Footer.jsx
@@ -1,26 +1,31 @@
 import { Link as RouterLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 // material-ui
 import { Link, Stack, Typography } from '@mui/material';
 
-import footerImg from 'assets/images/eume-logo-bottom.png'
+import footerImg from 'assets/images/eume-logo-bottom.png';
 
-const Footer = () => (
-  <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ p: '24px 16px 0px', mt: 'auto' }}>
-    <Typography variant="caption">&copy; All rights reserved by EUME</Typography>
-    <Stack spacing={1.5} direction="row" justifyContent="space-between" alignItems="center">
-      <img src={footerImg} alt="EUME-logo" />
-      {/* <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
-        About us
-      </Link> */}
-      {/* <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
-        Privacy
-      </Link>
-      <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
-        Terms
-      </Link> */}
+const Footer = () => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ p: '24px 16px 0px', mt: 'auto' }}>
+      <Typography variant="caption">&copy; {t("All rights reserved by Perfect Numbers")}</Typography>
+      <Stack spacing={1.5} direction="row" justifyContent="space-between" alignItems="center">
+        <img src={footerImg} alt="EUME-logo" />
+        {/* <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
+          {t("About us")}
+        </Link> */}
+        {/* <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
+          {t("Privacy")}
+        </Link>
+        <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
+          {t("Terms")}
+        </Link> */}
+      </Stack>
     </Stack>
-  </Stack>
-);
+  );
+};
 
 export default Footer;
