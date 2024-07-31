70adafc0d747d066f4d6c097efb2e8c2709b97ab
Ishara <isharacsgl@gmail.com>
2024-07-31 19:40:48 +0530

    useEffect error

--- src/pages/eume/EditableTable.jsx
+++ src/pages/eume/EditableTable.jsx
@@ -110,17 +110,6 @@ const EditableTable = ({ data }) => {
             item.noOfDays.includes(searchValue)
         );
 
-  useEffect(() => {
-    const getEmpList = async () => {
-      try {
-        const municipalities = await fetchMunicipalities(user?.role);
-        const data = await getDocs(empCollectionRef);
-        const filteredData = data?.docs?.map((doc) => ({
-          ...doc.data(),
-          id: doc.id
-        }));
-
-        let searchedData = filteredData.filter((item) => municipalities.includes(item.municipality));
 
         if (searchValue) {
           searchedData = searchedData.filter(
@@ -138,9 +127,7 @@ const EditableTable = ({ data }) => {
           );
         }
 
-        setEmpList(searchedData);
-      } catch (err) {
-        console.log(err);
+   
 
       }
       if (selectedGender) {
